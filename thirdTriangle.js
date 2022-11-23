a1a7127194d58c066b635f86b23d8af540aab4d1
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-11-22 22:42:01 -0500

    created js file with code

---
+++ thirdTriangle.js
@@ -0,0 +1,5 @@
+//created a variable named C and adding a + b and subtract it to 180 get my 3rd angle. 180 is the max of a triangle formula. So i return c
+function otherAngle(a, b) {
+  let c = 180 - (a + b) 
+  return c;
+}
